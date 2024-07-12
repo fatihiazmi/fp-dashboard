1a23f5f62be7f78159366b667b77304110bd266c
fatihiazmi <70769424+fatihiazmi@users.noreply.github.com>
2024-07-12 15:47:16 +0800

    update: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                port: '',
-                pathname: '/v0/b/leaderboard-48dfb.appspot.com/o/**'
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/leaderboard-48dfb.appspot.com/o/**",
+      },
+    ],
+  },
+  distDir: "build",
 };
 
 export default nextConfig;
